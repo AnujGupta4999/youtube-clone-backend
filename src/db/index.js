655430e56feeb7acf1ade3db7a1ec60fa6565769
Anuj Gupta <89295349+AnujGupta4999@users.noreply.github.com>
2024-04-09 12:03:42 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,27 +1,5 @@
-// process.setMaxListeners(15);
-
-// import mongoose from "mongoose";
-
-// import { DB_NAME } from '../constant.js';
-
-// const connectDB = async ()=> {
-//     try{
-//         const connectionInstance = await mongoose.connect("mongodb://localhost:27017/youtube-clone",  {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connect.host}`);
-//     }catch(error){
-//         console.log("Mongodb connection error: " ,error);
-//         process.exit(1);
-//     }
-// }
-
-// export default connectDB;
-
 
 import mongoose from "mongoose";
-// import { DB_NAME } from '../constant.js';
 
 const connectDB = async () => {
     try {
